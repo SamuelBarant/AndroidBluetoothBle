e4b97e0a2d9d7f4b8803bdad05cddbeca59048a4
SamuelBarant <samucraft346@gmail.com>
2026-01-19 13:10:12 +0100

    Theme corrected.

--- app/src/main/java/barant/curso/androidbluetoothble/core/ui/theme/Color.kt
+++ app/src/main/java/barant/curso/androidbluetoothble/core/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package barant.curso.androidbluetoothble.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Colores principales de la app
+val DarkBackground = Color(0xAB1B1C23)  // Fondo general
+val SurfaceColor = Color(0xFFFFFFFF)    // Fondo de cards, topBar, bottomBar
+val PrimaryCyan = Color(0xFF00DEC8)     // Primario / acentos
+val SecundaryCyan = Color(0xFF204A46)     // Primario / acentos
+val TextWhite = Color(0xFFFAFAFA)       // Texto principal
+val TextSecondary = Color(0xFF9BA1A6)   // Texto secundario
+val DisconnectRed = Color(0xFFFF5252)   // Error / desconexión
--- app/src/main/java/barant/curso/androidbluetoothble/core/ui/theme/Type.kt
+++ app/src/main/java/barant/curso/androidbluetoothble/core/ui/theme/Type.kt
@@ -6,29 +6,30 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        letterSpacing = 2.sp,
+        color = TextWhite
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = TextSecondary
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        color = TextWhite
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        color = TextWhite
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
